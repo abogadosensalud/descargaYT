f1f4c23431d2039ff7e646bbc72bb33bae09e79d
abogadosensalud <abogadosensaludarg@gmail.com>
2025-06-25 15:58:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,6 @@ def serve_file(filename):
     return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
